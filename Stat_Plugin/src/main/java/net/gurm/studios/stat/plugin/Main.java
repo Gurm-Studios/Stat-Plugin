9f204fae08fa9cbfa6db15d1e3e8d70cd1561381
SOBI <96914208+Quin0019@users.noreply.github.com>
2022-03-26 17:41:34 +0900

    Update Main.java

--- Stat_Plugin/src/main/java/net/gurm/studios/stat/plugin/Main.java
+++ Stat_Plugin/src/main/java/net/gurm/studios/stat/plugin/Main.java
@@ -1,4 +1,5 @@
 package net.gurm.studios.stat.plugin;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -9,19 +10,20 @@ public class Main extends JavaPlugin {
     public void onEnable(){
         getLogger().info("★Plugin [✔]");
     }
+    
     //CommandSender sender,Command command,String label,String[] args커맨드
     public boolean onCommand(CommandSender sender,Command command,String label,String[] args){
         if(label.equalsIgnoreCase("스텟 명령어")){
             Player player=(Player)sender;
+            
             player.sendMessage("스텟 player add 스텟종류 수치   수치만큼 스텟을 올립니다\n"+
                     "");
         }
         return true;
     }
+    
     public void onDisable(){
         getLogger().info("★Plugin [❌]");
     }
-
-
 
 }
