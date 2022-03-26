3e67f153e297083a810b30754a3b649aad276a00
SOBI <96914208+Quin0019@users.noreply.github.com>
2022-03-26 20:44:43 +0900

    Update Start.java

--- src/main/java/net/gurm/studios/stat/plugin/commands/Start.java
+++ src/main/java/net/gurm/studios/stat/plugin/commands/Start.java
@@ -5,7 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class start implements CommandExecutor  {
+public class Start implements CommandExecutor  {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
