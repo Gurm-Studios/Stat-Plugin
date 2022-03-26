6447b5df1b94e7104c2265544b9db7ae45565efa
SOBI <96914208+Quin0019@users.noreply.github.com>
2022-03-26 20:45:04 +0900

    Update Main.java

--- src/main/java/net/gurm/studios/stat/plugin/Main.java
+++ src/main/java/net/gurm/studios/stat/plugin/Main.java
@@ -1,4 +1,5 @@
 package net.gurm.studios.stat.plugin;
+
 import net.gurm.studios.stat.plugin.commands.start;
 import net.gurm.studios.stat.plugin.commands.stat;
 import org.bukkit.Bukkit;
@@ -9,19 +10,18 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class Main extends JavaPlugin {
+    
     ConsoleCommandSender consol = Bukkit.getConsoleSender();
     HashMap<UUID, Integer> map = new HashMap<UUID, Integer>();
 
     public void onEnable(){
         getLogger().info("★Plugin [✔]");
-        getCommand("스텟").setExecutor(new stat());
-        getCommand("시작").setExecutor(new start());
+        getCommand("스텟").setExecutor(new Stat());
+        getCommand("시작").setExecutor(new Start());
     }
     public void onDisable(){
         getLogger().info("★Plugin [❌]");
     }
-
-
 
 
 }
