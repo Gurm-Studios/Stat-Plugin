bf5df477f5af4d320174580664d18fa088668ec1
SOBI <96914208+Quin0019@users.noreply.github.com>
2022-03-30 13:03:38 +0900

    Update Main.java

--- src/main/java/net/gurm/studios/stat/plugin/Main.java
+++ src/main/java/net/gurm/studios/stat/plugin/Main.java
@@ -1,7 +1,7 @@
 package net.gurm.studios.stat.plugin;
 
-import net.gurm.studios.stat.plugin.commands.start;
-import net.gurm.studios.stat.plugin.commands.stat;
+import net.gurm.studios.stat.plugin.commands.Start;
+import net.gurm.studios.stat.plugin.commands.Stat;
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
