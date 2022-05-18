7b754a58573b8ad3993f9e1576d8f0e4dfcebb96
honmono9999 <98643150+honmono9999@users.noreply.github.com>
2022-05-18 18:00:52 +0900

    Add files via upload

---
+++ src/main/java/net/gurm/studios/stat/plugin/GUI.java
@@ -0,0 +1,127 @@
+package net.gurm.studios.stat.plugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GUI {
+    public Stat s = new Stat();
+
+    public GUI() {
+    }
+
+    public void Stack(String display, int ID, int Data, int Stack, List<String> lore, int loc, Inventory inv) {
+        ItemStack Item = (new MaterialData(ID, (byte)Data)).toItemStack(Stack);
+        ItemMeta Item_meta = Item.getItemMeta();
+        Item_meta.setDisplayName(display);
+        Item_meta.setLore(lore);
+        Item.setItemMeta(Item_meta);
+        inv.setItem(loc, Item);
+    }
+
+    public void SGUI(Player p,Player sp) {
+        long[] i;
+        i = s.readstat(p.getUniqueId().toString());
+        Inventory inv = Bukkit.createInventory(null, 54, ChatColor.DARK_PURPLE+"스테이터스");
+        double percentage=(double) i[1]/(double) i[2]*100;
+        Stack(ChatColor.BOLD + ""+ChatColor.DARK_PURPLE + p.getPlayerListName(), 384, 0, 1,
+                Arrays.asList("레벨:" + i[0], "경험치:" + i[1] + "/" + i[2]+"("+percentage+"%)",
+                        "스텟포인트:"+i[3],
+                        "제한 스텟포인트:"+i[4],
+                        "소지금:"+i[17]), 4, inv);
+        Stack(ChatColor.BOLD + "" + ChatColor.DARK_RED + "힘",276,0,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[5]),20,inv
+                );
+        Stack(ChatColor.BOLD + "" + ChatColor.DARK_RED + "힘(제한 스텟)",276,0,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[6]),29,inv
+        );
+        Stack(ChatColor.BOLD + "" + ChatColor.YELLOW + "민첩",288,0,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[7]),38,inv
+        );
+        Stack(ChatColor.BOLD + "" + ChatColor.YELLOW + "민첩(제한 스텟)",288,0,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[8]),47,inv
+        );
+        Stack(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "맷집",311,0,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[9]),22,inv
+        );
+        Stack(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "맷집(제한 스텟)",311,0,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[10]),31,inv
+        );
+        Stack(ChatColor.BOLD + "" + ChatColor.RED + "생명력",322,1,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[11]),40,inv
+        );
+        Stack(ChatColor.BOLD + "" + ChatColor.RED + "생명력(제한 스텟)",322,1,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[12]),49,inv
+        );
+        Stack(ChatColor.BOLD + "" + ChatColor.BLUE + "마나",351,4,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[13]),24,inv
+        );
+        Stack(ChatColor.BOLD + "" + ChatColor.BLUE + "마나(제한 스텟)",351,4,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[14]),33,inv
+        );
+        Stack(ChatColor.BOLD + "" + ChatColor.DARK_PURPLE + "마력",351,12,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[15]),42,inv
+        );
+        Stack(ChatColor.BOLD + "" + ChatColor.DARK_PURPLE + "마력(제한 스텟)",351,12,1,
+                Arrays.asList(ChatColor.DARK_GRAY+"현재 스텟:"+i[16]),51,inv
+        );
+        sp.openInventory(inv);
+    }
+
+    public void SGUI_click(InventoryClickEvent e)
+    {
+        e.setCancelled(true);
+        Player p=(Player) e.getWhoClicked();
+        if(ChatColor.stripColor(e.getInventory().getTitle()).equalsIgnoreCase("스테이터스"))
+        {
+            if(e.getCurrentItem()==null||
+                    e.getCurrentItem().getType()== Material.AIR||
+                    e.getCurrentItem().hasItemMeta())
+            {
+                return;
+            }
+            else
+            {
+                long[] i;
+                i=s.readstat(p.getUniqueId().toString());
+                switch (ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName()))
+                {
+                    case "힘":s.Stat_up(i,p,5);
+                        break;
+                    case "힘(제한 스텟)":s.Stat_up(i,p,6);
+                        break;
+                    case "민첩":s.Stat_up(i,p,7);
+                        break;
+                    case "민첩(제한 스텟)":s.Stat_up(i,p,8);
+                        break;
+                    case "맷집":s.Stat_up(i,p,9);
+                        break;
+                    case "맷집(제한 스텟)":s.Stat_up(i,p,10);
+                        break;
+                    case "생명력":s.Stat_up(i,p,11);
+                        break;
+                    case "생명력(제한 스텟)":s.Stat_up(i,p,12);
+                        break;
+                    case "마나":s.Stat_up(i,p,13);
+                        break;
+                    case "마나(제한 스텟)":s.Stat_up(i,p,14);
+                        break;
+                    case "마력":s.Stat_up(i,p,15);
+                        break;
+                    case "마력(제한 스텟)":s.Stat_up(i,p,16);
+                        break;
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/net/gurm/studios/stat/plugin/commands/Stat.java
@@ -0,0 +1,52 @@
+package net.gurm.studios.stat.plugin.commands;
+
+import net.gurm.studios.stat.plugin.GUI;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Stat implements CommandExecutor {
+    public net.gurm.studios.stat.plugin.Stat s =new net.gurm.studios.stat.plugin.Stat();
+    public GUI gui=new GUI();
+    //CommandSender sender,Command command,String label,String[] args커맨드
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        Player player=(Player)sender;
+        if(sender instanceof Player) {
+            if (((args[0].equalsIgnoreCase("명령어")) || args[0].equalsIgnoreCase("command")) && sender.isOp()) {
+                player.sendMessage(
+                        "스텟" + ChatColor.YELLOW + "player" + ChatColor.GOLD + "add" +
+                                ChatColor.RESET + "스텟종류 수치\n" +
+                                ChatColor.GRAY + "   수치만큼 스텟을 올립니다\n" +
+
+                                "스텟" + ChatColor.YELLOW + "player" + ChatColor.GOLD + "min" +
+                                ChatColor.RESET + "스텟종류 수치\n" +
+                                ChatColor.GRAY + "   수치만큼 스텟을 내립니다\n" +
+
+                                "스텟" + ChatColor.YELLOW + "player" + ChatColor.GOLD +
+                                ChatColor.RESET + "스텟종류 수치\n" +
+                                ChatColor.GRAY + "   수치만큼 스텟을 정합니다\n" +
+
+                                "스텟" + ChatColor.GOLD + "확인" + ChatColor.YELLOW + "player\n" +
+                                ChatColor.GRAY + "   player의 스텟을 확인 합니다\n");
+            } else if (args.length == 0) {
+                gui.
+            } else if (args[0].equalsIgnoreCase("확인") || args[0].equalsIgnoreCase("check") && args.length == 2) {
+                long[] stat;
+                stat = s.readstat(String.valueOf(Bukkit.getPlayer(args[1]).getUniqueId()));
+                player.sendMessage("레벨:" + stat[0]
+                        + "\n힘" + stat[5]
+                        + "\n민첩" + stat[6]
+                        + "\n멧집" + stat[7]
+                        + "\n생명력" + stat[8]
+                        + "\n마나" + stat[9]
+                        + "\n마력" + stat[10]);
+            } else {
+                player.sendMessage("버~그");
+            }
+        }
+        return false;
+    }
+}
